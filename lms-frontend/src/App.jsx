02e2a2b5b6d7c5605d4594f745212f7f70b2543f
Ashutosh Dangi <ashutoshdangirdps@gmail.com>
2023-09-11 23:16:52 +0530

    Redux Setup with Toolkit Done

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // useEffect(() => {
+  //   toast.error("hello");
+  // } )
 
   return (
     <h1 className='text-3xl font-bold underline' >
